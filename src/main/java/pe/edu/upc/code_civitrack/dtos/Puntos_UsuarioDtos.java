e5eb4ace526af661646d432649651a3fd844b749
Katherine <148381313+bxxnis@users.noreply.github.com>
2025-09-16 17:21:20 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/upc/code_civitrack/dtos/Puntos_UsuarioDtos.java
@@ -0,0 +1,63 @@
+package pe.edu.upc.civitrack.Dtos;
+
+import pe.edu.upc.civitrack.Entities.Usuario;
+
+import java.time.LocalDateTime;
+
+public class Puntos_UsuarioDtos {
+
+    private int idPuntos;
+    private int puntosTotal;
+    private String nivelActual;
+    private LocalDateTime fechaInicio;
+    private LocalDateTime fechaCaducidad;
+    private Usuario usuario;
+
+    public int getIdPuntos() {
+        return idPuntos;
+    }
+
+    public void setIdPuntos(int idPuntos) {
+        this.idPuntos = idPuntos;
+    }
+
+    public int getPuntosTotal() {
+        return puntosTotal;
+    }
+
+    public void setPuntosTotal(int puntosTotal) {
+        this.puntosTotal = puntosTotal;
+    }
+
+    public String getNivelActual() {
+        return nivelActual;
+    }
+
+    public void setNivelActual(String nivelActual) {
+        this.nivelActual = nivelActual;
+    }
+
+    public LocalDateTime getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDateTime fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDateTime getFechaCaducidad() {
+        return fechaCaducidad;
+    }
+
+    public void setFechaCaducidad(LocalDateTime fechaCaducidad) {
+        this.fechaCaducidad = fechaCaducidad;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
